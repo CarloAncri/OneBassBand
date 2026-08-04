2d0ca4bba8290513b97a6f7074b9bbe61e7cfe8c
Carlo-Unimi <carlo.ancri@studenti.unimi.it>
2026-08-04 12:35:45 +0200

    octaver class

---
+++ Source/DSP/Octaver.cpp
@@ -0,0 +1,1 @@
+#include "Octaver.h"
--- Source/DSP/Octaver.h
+++ Source/DSP/Octaver.h
@@ -1,1 +1,16 @@
+#pragma once
 
+#include <JuceHeader.h>
+
+class Octaver
+{
+public:
+  Octaver() = default;
+  ~Octaver() = default;
+
+  void prepareToPlay(double sampleRate, int samplesPerBlock) {}
+  void processBlock(juce::AudioBuffer<float>& buffer) {}
+
+private:
+
+};
