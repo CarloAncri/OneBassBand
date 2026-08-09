be12c815ea505f3d13d2bb8785104e546933e00e
Carlo-Unimi <carlo.ancri@studenti.unimi.it>
2026-08-09 17:52:34 +0200

    Distortion class (provv)

---
+++ Source/DSP/Distortion.cpp
@@ -0,0 +1,51 @@
+#include "Distortion.h"
+#include <cmath>
+
+
+Distortion::Distortion(float distAmount)
+{
+  setDistortionAmount(distAmount);
+}
+
+
+void Distortion::prepareToPlay(double sampleRate, int samplesPerBlock)
+{
+  juce::ignoreUnused(sampleRate, samplesPerBlock);
+}
+
+
+void Distortion::setDistortionAmount(float newAmount)
+{
+  amount = juce::jlimit(0.0f, 1.0f, newAmount);
+  thr = 1.0f - (amount * 0.95f);
+  drive = 1.0f / thr;
+}
+
+
+void Distortion::processBlock(juce::dsp::AudioBlock<float> &block)
+{
+  if (amount == 0.0f)
+    return;
+
+  const size_t numChannels = block.getNumChannels();
+  const size_t numSamples = block.getNumSamples();
+
+  const float asymmetry = 0.1f * amount;
+  const float dcOffsetCompensation = std::tanh(asymmetry);
+  const float makeupGain = 1.0f - (amount * 0.3f);
+
+  for (size_t ch = 0; ch < numChannels; ++ch)
+  {
+    float *channelData = block.getChannelPointer(ch);
+
+    for (size_t i = 0; i < numSamples; ++i)
+    {
+      float x = channelData[i];
+      float drivenX = (x * drive) + asymmetry;
+      float distorted = std::tanh(drivenX);
+      distorted -= dcOffsetCompensation;
+
+      channelData[i] = distorted * makeupGain;
+    }
+  }
+}
--- Source/DSP/Distortion.h
+++ Source/DSP/Distortion.h
@@ -1,1 +1,23 @@
+#pragma once
 
+#include <JuceHeader.h>
+
+#include "../PluginParameters.h"
+
+
+class Distortion
+{
+public:
+  Distortion(float distAmount = Parameters::defaultDistortionAmount);
+  ~Distortion() = default;
+
+  void prepareToPlay(double sampleRate, int samplesPerBlock);
+
+  void setDistortionAmount(float newAmount);
+
+  void processBlock(juce::dsp::AudioBlock<float> &buffer);
+private:
+  float amount = 0.0f;
+  float thr = 1.0f;
+  float drive = 1.0f;
+};
