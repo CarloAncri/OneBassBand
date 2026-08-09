e3f54739ee34e6d8484fe178b36244ffffc25550
Carlo-Unimi <carlo.ancri@studenti.unimi.it>
2026-08-09 12:18:04 +0200

    Octaver parameter and dsp block

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -4,15 +4,16 @@
 //==============================================================================
 OneBassBandAudioProcessor::OneBassBandAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
+    : AudioProcessor(BusesProperties()
+#if !JucePlugin_IsMidiEffect
+#if !JucePlugin_IsSynth
+                         .withInput("Input", juce::AudioChannelSet::stereo(), true)
 #endif
+                         .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+                         ),
+#endif
+      apvts(*this, nullptr, "PARAMETERS", Parameters::createParameterLayout())
 {
 }
 
@@ -23,149 +24,119 @@ OneBassBandAudioProcessor::~OneBassBandAudioProcessor()
 //==============================================================================
 const juce::String OneBassBandAudioProcessor::getName() const
 {
-    return JucePlugin_Name;
+  return JucePlugin_Name;
 }
 
 bool OneBassBandAudioProcessor::acceptsMidi() const
 {
-   #if JucePlugin_WantsMidiInput
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_WantsMidiInput
+  return true;
+#else
+  return false;
+#endif
 }
 
 bool OneBassBandAudioProcessor::producesMidi() const
 {
-   #if JucePlugin_ProducesMidiOutput
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_ProducesMidiOutput
+  return true;
+#else
+  return false;
+#endif
 }
 
 bool OneBassBandAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_IsMidiEffect
+  return true;
+#else
+  return false;
+#endif
 }
 
 double OneBassBandAudioProcessor::getTailLengthSeconds() const
 {
-    return 0.0;
+  return 0.0;
 }
 
 int OneBassBandAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+  return 1;
 }
 
 int OneBassBandAudioProcessor::getCurrentProgram()
 {
-    return 0;
+  return 0;
 }
 
-void OneBassBandAudioProcessor::setCurrentProgram (int index)
+void OneBassBandAudioProcessor::setCurrentProgram(int index)
 {
 }
 
-const juce::String OneBassBandAudioProcessor::getProgramName (int index)
+const juce::String OneBassBandAudioProcessor::getProgramName(int index)
 {
-    return {};
+  return {};
 }
 
-void OneBassBandAudioProcessor::changeProgramName (int index, const juce::String& newName)
+void OneBassBandAudioProcessor::changeProgramName(int index, const juce::String &newName)
 {
 }
-
-
-
 
 //==============================================================================
-void OneBassBandAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void OneBassBandAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
-
+  octaver.prepareToPlay(sampleRate, samplesPerBlock);
 }
 
 void OneBassBandAudioProcessor::releaseResources()
 {
-
 }
 
-#ifndef JucePlugin_PreferredChannelConfigurations
-bool OneBassBandAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+void OneBassBandAudioProcessor::processBlock(juce::AudioBuffer<float> &buffer, juce::MidiBuffer &midiMessages)
 {
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
+  juce::ScopedNoDenormals noDenormals;
 
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
+  // === parameters update ===
+  auto *octaveParam = apvts.getRawParameterValue(Parameters::pitchShiftedOctave);
 
-    return true;
-  #endif
-}
-#endif
-
-void OneBassBandAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
-{
-    juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
-
-
+  
+  // === processing ===
+  if (octaveParam != nullptr)
+  {
+    int currentOctave = static_cast<int>(octaveParam->load());
+    octaver.setOctave(currentOctave);
+  }
+  octaver.processBlock(buffer);
 }
 
 //==============================================================================
 bool OneBassBandAudioProcessor::hasEditor() const
 {
-    return false; // (change this to false if you choose to not supply an editor)
+  return false;
 }
 
-juce::AudioProcessorEditor* OneBassBandAudioProcessor::createEditor()
+juce::AudioProcessorEditor *OneBassBandAudioProcessor::createEditor()
 {
-    return new OneBassBandAudioProcessorEditor (*this);
+  return new OneBassBandAudioProcessorEditor(*this);
 }
 
 //==============================================================================
-void OneBassBandAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+void OneBassBandAudioProcessor::getStateInformation(juce::MemoryBlock &destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+  // You should use this method to store your parameters in the memory block.
+  // You could do that either as raw data, or use the XML or ValueTree classes
+  // as intermediaries to make it easy to save and load complex data.
 }
 
-void OneBassBandAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void OneBassBandAudioProcessor::setStateInformation(const void *data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+  // You should use this method to restore your parameters from this memory block,
+  // whose contents will have been created by the getStateInformation() call.
 }
-
-
-
-
-
-
-
 
 //==============================================================================
 // This creates new instances of the plugin..
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+juce::AudioProcessor *JUCE_CALLTYPE createPluginFilter()
 {
-    return new OneBassBandAudioProcessor();
+  return new OneBassBandAudioProcessor();
 }
